4facf43baee2ebba4152e37e310b3739a9e7e0d2
doremifasolasi <lena.lenova.2014@gmail.com>
2021-03-18 22:43:22 +0200

    Added file-html for loging

--- flask/Attempt_to_use/venv/my_CV.py
+++ flask/Attempt_to_use/venv/my_CV.py
@@ -1,6 +1,8 @@
 from flask import Flask
-from markupsafe import escape
-from flask import redirect
+# from markupsafe import escape
+# from flask import redirect
+from flask import render_template
+
 
 # from flask import Flask, escape, url_for
 
@@ -47,21 +49,29 @@ app = Flask(__name__) # створюємо екземпляр класу з па
 
 # To redirect a user to another endpoint, use the redirect() function
 
-@app.route("/admin")
-def hello_admin():
-    return "Hello, Admin!!!"
+# @app.route("/admin")
+# def hello_admin():
+#     return "Hello, Admin!!!"
 
-@app.route("/guest/<guest>")
-def hello_guest(guest): 
-    return f"Hello {guest} as Guest"
+# @app.route("/guest/<guest>")
+# def hello_guest(guest): 
+#     return f"Hello {guest} as Guest"
 
-@app.route("/user/<name>")
-def hello_user(name): 
-    if name=="admin":
-        return redirect(url_for("hello_admin"))  # from flask import redirect
-    else:
-        return redirect(url_for("hello_guest", guest=name))
+# @app.route("/user/<name>")
+# def hello_user(name): 
+#     if name=="admin":
+#         return redirect(url_for("hello_admin"))  # from flask import redirect
+#     else:
+#         return redirect(url_for("hello_guest", guest=name))
 
-if __name__ == '__main__': # для автоматичного дебагінгу
-    app.run(debug=True)
+# if __name__ == '__main__': # для автоматичного дебагінгу
+#     app.run(debug=True)
+
+
+# Rendering Templates
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name) # from flask import render_template
 
