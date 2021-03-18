efcfc51c0b7ffab15e49b3247c996ef2361509d8
doremifasolasi <lena.lenova.2014@gmail.com>
2021-03-18 20:47:59 +0200

    Add variable sections to a URL by marking sections with <variable_name>

--- flask/Attempt_to_use/venv/my_CV.py
+++ flask/Attempt_to_use/venv/my_CV.py
@@ -14,10 +14,30 @@ app = Flask(__name__) # створюємо екземпляр класу з па
 # $ flask run
 #  * Running on http://127.0.0.1:5000/
 
-@app.route('/')
-def index():
-    return 'Index Page'
+# Use the route() decorator to bind a function to a URL.
 
-@app.route('/hello')
-def hello():
-    return 'Hello, World'
+# @app.route('/')
+# def index():
+#     return 'Index Page'
+
+# @app.route('/hello')
+# def hello():
+#     return 'Hello, World'
+
+# Add variable sections to a URL by marking sections with <variable_name>.
+
+@app.route('/user/<username>') # <динамічна змінна>
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % escape(username)
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    # show the subpath after /path/
+    return 'Subpath %s' % escape(subpath)
+
