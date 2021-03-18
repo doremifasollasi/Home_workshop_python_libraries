ae47e1b9767f769ce1c668298d6185357e2e2a82
doremifasolasi <lena.lenova.2014@gmail.com>
2021-03-18 20:31:47 +0200

    Use the route() decorator to bind a function to a URL.

--- flask/Attempt_to_use/venv/my_CV.py
+++ flask/Attempt_to_use/venv/my_CV.py
@@ -3,6 +3,21 @@ from flask import Flask
 app = Flask(__name__) # створюємо екземпляр класу з параметром __name__
 
 
-@app.route('/') # декорую функцію, щоб виводити інформацію на веб-сторінку
-def hello_world():
-    return 'Hello, World!'
+# @app.route('/') # декорую функцію, щоб виводити інформацію на веб-сторінку
+# def hello_world():
+#     return 'Hello, my dear friend!'
+
+# cd ..
+# $ set FLASK_APP=my_CV.py
+# або
+# set FLASK_ENV=development  тоді буде lazy loading, а також Debugger is active!
+# $ flask run
+#  * Running on http://127.0.0.1:5000/
+
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World'
