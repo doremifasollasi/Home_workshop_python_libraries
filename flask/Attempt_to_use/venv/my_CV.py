22bab5a297fb9b6a52c98c5e79a9747b11e60bc7
doremifasolasi <lena.lenova.2014@gmail.com>
2021-03-18 19:43:57 +0200

    Created function and added under it decorator

--- flask/Attempt_to_use/venv/my_CV.py
+++ flask/Attempt_to_use/venv/my_CV.py
@@ -1,3 +1,8 @@
 from flask import Flask
 
 app = Flask(__name__) # створюємо екземпляр класу з параметром __name__
+
+
+@app.route('/') # декорую функцію, щоб виводити інформацію на веб-сторінку
+def hello_world():
+    return 'Hello, World!'
