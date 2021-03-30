f9beff495a22e5615d920307b95a14f45ce200fd
doremifasollasi <lena.lenova.2014@gmail.com>
2021-03-30 12:58:41 +0300

    tried to add an empty object

--- SQLAlchemy/run.py
+++ SQLAlchemy/run.py
@@ -22,7 +22,18 @@ metadata.create_all(engine)
 #######################################
 
 insert_stmt = authors_table.insert(bind=engine) #engine може бути кілька різних, тому я вказую, якої саме бази даних стосуватиметься команда insert
-type(insert_stmt) #<class 'sqlalchemy.sql.expression.Insert'>
-print (insert_stmt) #INSERT INTO authors (id, name) VALUES (:id, :name)
+print(type(insert_stmt)) #<class 'sqlalchemy.sql.expression.Insert'>
+print(insert_stmt) #INSERT INTO authors (id, name) VALUES (:id, :name)
 
+compiled_stmt = insert_stmt.compile() #tried to add an empty object
+print(compiled_stmt.params) #{'id': None, 'name': None}
 
+# insert_stmt.execute(name='Alexandre Dumas') # insert a single entry
+# insert_stmt.execute([{'name': 'Mr X'}, {'name': 'Mr Y'}]) # a list of entries
+# metadata.bind = engine # no need to explicitly bind the engine from now on
+# select_stmt = authors_table.select(authors_table.c.id==2)
+# result = select_stmt.execute()
+# result.fetchall() #[(1, u'Mr X')]
+# del_stmt = authors_table.delete()
+# del_stmt.execute(whereclause=text("name='Mr Y'"))
+# del_stmt.execute() # delete all
